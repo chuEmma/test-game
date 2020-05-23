f6feaeed79c0f940a508fa51dd03bfffc10cbb8b
chuEmma <52865362+chuEmma@users.noreply.github.com>
2020-05-22 20:54:15 -0700

    Added animation to movements
    Still need to fix pixel distortion

--- test-game/Assets/Scripts/PlayerController.cs
+++ test-game/Assets/Scripts/PlayerController.cs
@@ -7,10 +7,12 @@ public class PlayerController : MonoBehaviour
 
     public float moveSpeed; 
 
+    private Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,5 +21,12 @@ public class PlayerController : MonoBehaviour
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) {
            transform.Translate (new Vector3 (Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));     
         }
+
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f) {
+           transform.Translate (new Vector3 (0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));     
+        }
+
+        anim.SetFloat("MoveX",Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("MoveY",Input.GetAxisRaw("Vertical"));
     }
 }
