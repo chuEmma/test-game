371739688c902c12135bc352646d83194adc3585
chuEmma <52865362+chuEmma@users.noreply.github.com>
2020-05-23 15:14:51 -0700

    Creating idle facing
    Included blend trees for animations

--- test-game/Assets/Scripts/PlayerController.cs
+++ test-game/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,9 @@ public class PlayerController : MonoBehaviour
 
     private Animator anim;
 
+    private bool playerMoving; 
+    private Vector2 lastMove; //x and y value
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +21,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        playerMoving = false; 
+
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) {
-           transform.Translate (new Vector3 (Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));     
+            transform.Translate (new Vector3 (Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));     
+            playerMoving = true;
+            lastMove = new Vector2 (Input.GetAxisRaw("Horizontal"), 0f); 
         }
 
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f) {
-           transform.Translate (new Vector3 (0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));     
+            transform.Translate (new Vector3 (0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));     
+            playerMoving = true;
+            lastMove = new Vector2 (0f, Input.GetAxisRaw("Vertical"));
         }
 
         anim.SetFloat("MoveX",Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("MoveY",Input.GetAxisRaw("Vertical"));
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
+        
     }
 }
